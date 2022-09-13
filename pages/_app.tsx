2c9e1c2060c5b63744cd4549f3582ea422cd8129
Théo Sabri <theo27400@live.fr>
2022-09-13 12:47:40 +0200

    Insert du projet pour vercel

---
+++ pages/_app.tsx
@@ -0,0 +1,24 @@
+// _app.tsx
+
+import { SessionProvider } from 'next-auth/react';
+import { AppProps } from 'next/app';
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
+
+export default App;
+
+/*import { AppProps } from "next/app";
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <Component {...pageProps} />
+  );
+};
+
+export default App;*/
